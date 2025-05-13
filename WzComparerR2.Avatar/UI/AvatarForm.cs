82b5b33b2be7a42908a9ca39ef50204a2c6b9a72
seotbeo <yoonv72@gmail.com>
2025-05-13 21:10:08 +0900

    Avatar: Fix EarType not set when load avatar using api

--- WzComparerR2.Avatar/UI/AvatarForm.cs
+++ WzComparerR2.Avatar/UI/AvatarForm.cs
@@ -1740,7 +1740,15 @@ namespace WzComparerR2.Avatar.UI
 
                 var mixFace = int.Parse(res.MixFaceRatio) != 0 ? $"+{res.MixFaceColor}*{res.MixFaceRatio}" : "";
                 var mixHair = int.Parse(res.MixHairRatio) != 0 ? $"+{res.MixHairColor}*{res.MixHairRatio}" : "";
-                this.avatar.EarType = res.EarType;
+
+                for (int i = 0; i < this.cmbEar.Items.Count; i++)
+                {
+                    if ((this.cmbEar.Items[i] as ComboItem).Text == res.EarType.ToString())
+                    {
+                        this.cmbEar.SelectedIndex = i;
+                        break;
+                    }
+                }
 
                 var code = $"20{res.Skin}, {res.Face + mixFace}, {res.Hair + mixHair}, {res.Cap}, {res.FaceAcc}, {res.EyeAcc}, {res.EarAcc}, {res.Coat}, {res.Pants}, {res.Shoes}, {res.Gloves}, {res.Cape}, {res.Shield}, {res.Weapon}, {res.CashWeapon}";
                 LoadCode(code, 0);
