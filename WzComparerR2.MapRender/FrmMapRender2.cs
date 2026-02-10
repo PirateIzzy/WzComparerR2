2870f74f2ea51ae4dc6b200fc3fed477aa005bea
seotbeo <yoonv72@gmail.com>
2026-02-10 22:15:55 +0900

    MapRender: add option to show foothold boundary

--- WzComparerR2.MapRender/FrmMapRender2.cs
+++ WzComparerR2.MapRender/FrmMapRender2.cs
@@ -137,6 +137,7 @@ namespace WzComparerR2.MapRender
         bool prepareCapture;
         bool captureViewPortOnly;
         bool ForceCaptureWithResolution;
+        bool showFootholdBoundary;
         Task captureTask;
         Resolution resolution;
         float opacity;
@@ -1378,6 +1379,7 @@ namespace WzComparerR2.MapRender
             this.batcher.D2DEnabled = config.UseD2dRenderer;
             (this.Content as WcR2ContentManager).UseD2DFont = config.UseD2dRenderer;
             this.ForceCaptureWithResolution = config.ForceCaptureWithResolution;
+            this.showFootholdBoundary = config.ShowFootholdBoundary;
         }
 
         private void LoadOptionData(UIOptionsDataModel model)
@@ -1395,6 +1397,7 @@ namespace WzComparerR2.MapRender
             model.Minimap_CameraRegionVisible = this.ui.Minimap.CameraRegionVisible;
             model.WorldMap_UseImageNameAsInfoName = this.ui.WorldMap.UseImageNameAsInfoName;
             model.ForceCaptureWithResolution = config.ForceCaptureWithResolution;
+            model.ShowFootholdBoundary = config.ShowFootholdBoundary;
             LoadCaptureRectOptionData(model);
         }
 
@@ -1414,6 +1417,7 @@ namespace WzComparerR2.MapRender
             config.Minimap_CameraRegionVisible = model.Minimap_CameraRegionVisible;
             config.WorldMap_UseImageNameAsInfoName = model.WorldMap_UseImageNameAsInfoName;
             config.ForceCaptureWithResolution = model.ForceCaptureWithResolution;
+            config.ShowFootholdBoundary = model.ShowFootholdBoundary;
             WzComparerR2.Config.ConfigManager.Save();
 
             if (int.TryParse(model.ScLeft, out int left) && int.TryParse(model.ScTop, out int top)
